99a45761c7c04431585378e0cecaae2cd8f526dd
Richard Melchior <raustin.melchior@gmail.com>
2018-05-29 06:01:17 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,4 +3,20 @@ class Dog
     @name = dog_name;
     @breed = dog_breed;
   end
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def name
+    "#{@name}".strip
+  end
+
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+
+  def breed
+    "#{@breed}".strip
+  end
 end
